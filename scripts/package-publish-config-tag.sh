57a222495e02ec63da239fd4af8a6a9ac44bc882
chengeng <chengeng@bloggeng.com>
2022-08-01 18:03:00 +0800

    fix(模块): 修改版本号

--- scripts/package-publish-config-tag.sh
+++ scripts/package-publish-config-tag.sh
@@ -7,6 +7,6 @@ if npx --package @chatie/semver semver-is-prod $VERSION; then
   npx pkg-jq -i '.publishConfig.tag="latest"'
   echo "production release: publicConfig.tag set to latest."
 else
-  npx pkg-jq -i '.publishConfig.tag="latest"'
+  npx pkg-jq -i '.publishConfig.tag="next"'
   echo 'development release: publicConfig.tag set to next.'
 fi
